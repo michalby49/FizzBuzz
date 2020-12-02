9ebecb85422ac364c19576375c7479d0d3cdc58c
michalby49 <beskamichal49@gmail.com>
2020-12-02 20:26:38 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public void CheckFizzBuzz(int number)
+        {
+           
+            if(number % 3 == 0 && number % 5 == 0)
+                Console.WriteLine("FizzBuzz");
+            else if(number % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if(number % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(number);
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Podaj liczbe: ");
+                int number = GetNumber();
+
+                var fizzBuzz = new FizzBuzz();
+
+                Console.Write("Wynik to: ");
+
+                fizzBuzz.CheckFizzBuzz(number);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static int GetNumber()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+                throw new Exception("To nie jest liczba");
+            return input;
+        }
+    }
+}
